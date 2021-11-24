93bd6afc8df50598290e53e3085548277f8bdc77
Mriganka Paul <mpaul@redhat.com>
2021-11-24 09:08:03 -0500

    Changing from topic to queue

--- src/main/java/amqcob/consumer/Consumer2.java
+++ src/main/java/amqcob/consumer/Consumer2.java
@@ -1,12 +1,8 @@
 package amqcob.consumer;
 
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.MessageConsumer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-import javax.jms.Topic;
+import javax.jms.*;
+import javax.naming.InitialContext;
 
 import org.apache.activemq.artemis.api.jms.ActiveMQJMSClient;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
@@ -15,23 +11,42 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 public class Consumer2 {
     public static void main(String[] args) throws Exception{
         Connection connection = null;
+        InitialContext initialContext = null;
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("##########################################################");
+        System.out.println("");
+        System.out.println("");
         try{
-            Topic topic = ActiveMQJMSClient.createTopic("exampleTopic");
-            ConnectionFactory cf = new ActiveMQConnectionFactory("tcp://localhost:61617");
-            connection = cf.createConnection();
+
+            initialContext = new InitialContext();
+            Queue queue = (Queue) initialContext.lookup("queue/exampleQueue");
+            ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
+            connection = connectionFactory.createConnection();
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             connection.start();
-            MessageConsumer consumer = session.createConsumer(topic);
+            MessageConsumer consumer = session.createConsumer(queue);
+            System.out.println("");
+            System.out.println("");
+            System.out.println("##########################################################");
+            System.out.println("");
+            System.out.println("Waiting for new messages in broker2.......");
 
-            final int numMessages = 10;
-
-            for (int i = 0; i < numMessages; i++) {
-                TextMessage message0 = (TextMessage) consumer.receive(50000);
-                System.out.println("Got message: " + message0.getText() + " from node 0");
+            while(true) {
+                TextMessage message = (TextMessage) consumer.receive(50000);
+                if(message != null) {
+                    System.out.println();
+                    System.out.println("Got Message: "+message.getText());
+                }
             }
+
         }finally {
             if(connection != null){
                 connection.close();
+            }
+            if(initialContext != null){
+                initialContext.close();
             }
         }
     }
